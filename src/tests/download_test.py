5dc18db05c2203ee6cb611b5840b40e81bd642f5
Tatu Laras <tatu.laras@gmail.com>
2024-11-28 17:38:55 +0200

    Fontawesome lokaaliksi

--- src/tests/download_test.py
+++ src/tests/download_test.py
@@ -1,13 +1,28 @@
 import unittest
-from entities.source import Source 
+from entities.source import Source
+
 
 class TestDownload(unittest.TestCase):
- 
+
     def test_lataa_yksi_lähde(self):
 
-        sources = ({"source_id":1,"bibtex_key":"bibtex_key_1","title":"bibtex_title_1","year":"2020","author":"bibtex_author_1"},
-                    {"source_id":2,"bibtex_key":"bibtex_key_2","title":"bibtex_title_2","year":"2021","author":"bibtex_author_2"})
-        
+        sources = (
+            {
+                "source_id": 1,
+                "bibtex_key": "bibtex_key_1",
+                "title": "bibtex_title_1",
+                "year": "2020",
+                "author": "bibtex_author_1",
+            },
+            {
+                "source_id": 2,
+                "bibtex_key": "bibtex_key_2",
+                "title": "bibtex_title_2",
+                "year": "2021",
+                "author": "bibtex_author_2",
+            },
+        )
+
         books = []
         for item in sources:
             books.append(Source(item))
@@ -15,12 +30,26 @@ class TestDownload(unittest.TestCase):
         bibtex = books[0].download()
         test = '@article {bibtex_key_1,\ntitle = "bibtex_title_1",\nyear = "2020",\nauthor = "bibtex_author_1"}\n\n'
         assert bibtex == test
-    
+
     def test_lataa_kaksi_lähdettä(self):
 
-        sources = ({"source_id":1,"bibtex_key":"bibtex_key_1","title":"bibtex_title_1","year":"2020","author":"bibtex_author_1"},
-                    {"source_id":2,"bibtex_key":"bibtex_key_2","title":"bibtex_title_2","year":"2021","author":"bibtex_author_2"})
-        
+        sources = (
+            {
+                "source_id": 1,
+                "bibtex_key": "bibtex_key_1",
+                "title": "bibtex_title_1",
+                "year": "2020",
+                "author": "bibtex_author_1",
+            },
+            {
+                "source_id": 2,
+                "bibtex_key": "bibtex_key_2",
+                "title": "bibtex_title_2",
+                "year": "2021",
+                "author": "bibtex_author_2",
+            },
+        )
+
         books = []
         for item in sources:
             books.append(Source(item))
@@ -30,3 +59,4 @@ class TestDownload(unittest.TestCase):
 
         test = '@article {bibtex_key_1,\ntitle = "bibtex_title_1",\nyear = "2020",\nauthor = "bibtex_author_1"}\n\n@article {bibtex_key_2,\ntitle = "bibtex_title_2",\nyear = "2021",\nauthor = "bibtex_author_2"}\n\n'
         assert bibtex == test
+
